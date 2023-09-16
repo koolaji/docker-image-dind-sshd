cf2de192ba63d28143b49e2467c0baaa1936048c
koolaji <koolaji>
2023-09-16 00:37:45 +0000

    podman

--- exe.sh
+++ exe.sh
@@ -1,7 +1,8 @@
-docker build --network host -t koolaji/dind-ssh-git:rhel-8-0.1 .
-docker rm -f  docker-sshd
-docker run -p 4848:22 --privileged --name docker-sshd --hostname docker-sshd -d docker.io/koolaji/dind-ssh-git:rhel-8-0.1
-sleep 5
-docker ps -a
-docker logs -f docker-sshd
+docker build --network host -f Dockerfile_podman -t koolaji/pdoman-ssh-git:rhel-8-0.1 .
+docker  push   koolaji/pdoman-ssh-git:rhel-8-0.1 
+#docker rm -f  podman-sshd
+#docker run -p 4848:22 --privileged --name podman-sshd --hostname podman-sshd --network bridge -d koolaji/pdoman-ssh-git:rhel-8-0.1
+#sleep 5
+#docker ps -a
+#docker logs -f podman-sshd
 #docker exec -ti docker-sshd passwd
---
+++ run_podman.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#SSHD
+# prepare keys
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+        # generate fresh rsa key
+        ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+fi
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+        # generate fresh dsa key
+        ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
+#prepare run dir
+if [ ! -d "/var/run/sshd" ]; then
+        mkdir -p /var/run/sshd
+fi
+
+# unlock root user for ssh user
+passwd -u root
+
+# start logging for ssh
+echo "starting syslog"
+syslogd &
+
+# start sshd
+echo "starting ssshd"
+/usr/sbin/sshd -D &
+
+mkdir -p /root/.ssh/
+
+touch /root/.ssh/authorized_keys
+
+chmod 600 /root/.ssh/authorized_keys
+
+# reread all config
+source /etc/profile
+
+# deploy authorized_keys
+if [ ! -z "${AUTHORIZED_KEYS}" ]; then
+    echo "${AUTHORIZED_KEYS//$'\n'/\n}" > /root/.ssh/authorized_keys
+fi
+
+# deploy docker-client-config ( docker registry login)
+if [ ! -z "${DOCKER_CLIENT_CONFIG_JSON}" ]; then
+    echo ${DOCKER_CLIENT_CONFIG_JSON} > /root/.docker/config.json
+fi
+
+if [ "$1" = 'docker' ]; then
+    echo "starting docker in client mode (BUSYWAIT), assuming you mounted /var/run/docker.sock from a running docker engine"
+    while :; do sleep 1; done
+    exit 0
+fi
+
+echo "starting dind"
+exec  "$@"
